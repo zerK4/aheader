f87e669b7bed3985c5b90f0c6be40f047f2040c8
zerK4 <sebastian.v.pavel@gmail.com>
2023-03-29 11:11:39 +0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className='min-h-screen bg-black text-sm text-gray-300 flex items-center justify-center'>
+    <Component {...pageProps} />
+  </div>
 }
