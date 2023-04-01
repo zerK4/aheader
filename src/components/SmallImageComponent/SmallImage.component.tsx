10bd336f38a33a1b092296fad51a1ce3b27b47e2
zerK4 <sebastian.v.pavel@gmail.com>
2023-04-01 14:38:02 +0300

    Ah-3: Refactor user popup component

--- src/components/SmallImageComponent/SmallImage.component.tsx
+++ src/components/SmallImageComponent/SmallImage.component.tsx
@@ -1,3 +1,4 @@
+import global from "@/state/global";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
@@ -6,8 +7,15 @@ import Loader from "../Loader/Loader.component";
 function SmallImageComponent() {
   const { data: { user: { image, name } = { image: "", name: "" } } = {} } =
     useSession();
+  const {
+    userPopup: { active },
+  } = global();
   return (
-    <div>
+    <div
+      className={`${
+        active ? "absolute left-2 top-2 opacity-30" : null
+      } ease-in-out duration-300`}
+    >
       {!image ? (
         <Loader />
       ) : (
--- src/lib/motionVariants/index.ts
+++ src/lib/motionVariants/index.ts
@@ -3,8 +3,8 @@ export const userMenuVariants = {
       opacity: 1,
       y: 0,
       transition: {
-        delay: i * 0.1,
-        duration: 0.2,
+        delay: i * 0.2,
+        duration: 0.4,
         ease: "easeOut",
       },
     }),
